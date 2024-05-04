e4f6207cb5e8ceeeb8bd756657ecc59b5785d571
V1rtualAlloc <kocovic.vladimir@gmail.com>
2024-05-04 18:38:04 +0200

    added jump pad

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Player
 
 @export var gravity: int = 400
 @export var jump_force: int = 200
@@ -14,7 +15,7 @@ func _physics_process(delta):
 		print("isn't on floor")
 	
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		velocity.y = -jump_force
+		jump(jump_force)
 	
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction != 0:
@@ -24,7 +25,10 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	update_animations(direction)
-	
+
+func jump(force):
+	velocity.y = -force
+
 func update_animations(direction):
 	if is_on_floor():
 		if direction == 0:
