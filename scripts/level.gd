0465093bec5496def42941750256f58316e7431f
V1rtualAlloc <kocovic.vladimir@gmail.com>
2024-05-05 03:30:05 +0200

    changing level on stepping into exit area

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
+@export var next_level: PackedScene = null
+
 @onready var start = $Start
+@onready var exit = $Exit
+
 var player = null
 
 func _ready():
@@ -11,6 +15,8 @@ func _ready():
 	for trap in traps:
 		#trap.connect("touched_player", _on_trap_touched_player)
 		trap.touched_player.connect(_on_trap_touched_player)
+	
+	exit.body_entered.connect(_on_exit_body_entered)
 
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
@@ -27,3 +33,11 @@ func _on_trap_touched_player():
 func reset_player():
 	player.velocity = Vector2.ZERO
 	player.position = start.get_spawn_pos()
+
+func _on_exit_body_entered(body):
+	if body is Player:
+		if next_level != null:
+			exit.animate()
+			player.active = false
+			await get_tree().create_timer(1.5).timeout
+			get_tree().change_scene_to_packed(next_level)
