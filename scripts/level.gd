35f2c71c41903bdf4ec0eb41a28dd921a707e7ea
V1rtualAlloc <kocovic.vladimir@gmail.com>
2024-05-05 03:50:42 +0200

    moved deathzone to scene, added new levels

--- scripts/level.gd
+++ scripts/level.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 @onready var start = $Start
 @onready var exit = $Exit
+@onready var death_zone = $Deathzone
 
 var player = null
 
@@ -17,6 +18,7 @@ func _ready():
 		trap.touched_player.connect(_on_trap_touched_player)
 	
 	exit.body_entered.connect(_on_exit_body_entered)
+	death_zone.body_entered.connect(_on_deathzone_body_entered)
 
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
